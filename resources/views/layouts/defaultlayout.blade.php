4830d8d6f7a483f7ad714567bb4be7d49cc5d387
Ahmad Sufi Bin Abdul Hamid <sufi@mindwave.my>
2022-11-09 22:10:36 +0800

    update default layout

--- resources/views/layouts/defaultlayout.blade.php
+++ resources/views/layouts/defaultlayout.blade.php
@@ -135,8 +135,8 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="{{ route('dashboard') }}" class="brand-link">
-                <img src="{{ url('images/TDDM.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                    style="opacity: .8">
+                <img src="{{ url('images/anylogo.png') }}" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">MSL9</span>
             </a>
 
@@ -174,7 +174,7 @@
         </div>
 
         <footer class="main-footer">
-            <strong>Copyright &copy; 2022 <a href="">SUFI REPO</a>.</strong>
+            <strong>Copyright &copy; 2022 <a href="https://sufirepository.github.io/">SUFI REPO</a>.</strong>
 
             <div class="float-right d-none d-sm-inline-block">
                 <b>Version</b> 1
