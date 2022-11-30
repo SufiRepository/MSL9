398fccd6d05355d88fdda2dec1df672533b6c62a
Ahmad Sufi Bin Abdul Hamid <sufi@mindwave.my>
2022-11-30 12:28:31 +0800

    update dashboard ui

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,25 +1,26 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="{{ route('dashboard') }}" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        {{-- <button type="button" class="btn btn-block btn-default" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</button>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="{{ route('dashboard') }}" class="nav-link">HOME</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            {{-- <button type="button" class="btn btn-block btn-default" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Log out</button>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;" >
               @csrf
           </form> --}}
-          <button type="button" class="btn btn-block btn-default" data-toggle="modal" data-target="#modal-sm">Log Keluar</button>
-      </li>
+            <button type="button" class="btn btn-block btn-default" data-toggle="modal" data-target="#modal-sm">Log
+                Keluar</button>
+        </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      {{-- <li class="nav-item">
+        <!-- Navbar Search -->
+        {{-- <li class="nav-item">
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
           <i class="fas fa-search"></i>
         </a>
@@ -42,41 +43,42 @@
 
 
 
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      {{-- <li class="nav-item">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+        {{-- <li class="nav-item">
         <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
           <i class="fas fa-th-large"></i>
         </a>
       </li> --}}
     </ul>
-  </nav>
+</nav>
 
-  <div class="modal fade" id="modal-sm">
+<div class="modal fade" id="modal-sm">
     <div class="modal-dialog modal-sm">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Log Keluar</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">LOG OUT</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>CONFIRM LOG OUT?</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">YES</button>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+                <button type="button" class="btn btn-default" data-dismiss="modal">CANCEL</button>
+            </div>
         </div>
-        <div class="modal-body">
-          <p>Sahkan log keluar?</p>
-        </div>
-        <div class="modal-footer justify-content-between">
-          <button type="button" class="btn btn-default" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Ya</button>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;" >
-                @csrf
-            </form>
-          <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-        </div>
-      </div>
-      <!-- /.modal-content -->
+        <!-- /.modal-content -->
     </div>
     <!-- /.modal-dialog -->
-  </div>
-  <!-- /.modal -->
+</div>
+<!-- /.modal -->
--- resources/views/layouts/sidebarmenu.blade.php
+++ resources/views/layouts/sidebarmenu.blade.php
@@ -7,46 +7,34 @@
                 <a href="#"
                     class="nav-link @isset($navlink) @if (in_array('pengurusan_pengguna', $navlink)) active @endif @endisset">
                     <i class="nav-icon fas fa-user-cog"></i>
-                    <p>PENGURUSAN PENGGUNA <i class="fas fa-angle-left right"></i> </p>
+                    <p>ADMIN PANELS<i class="fas fa-angle-left right"></i> </p>
                 </a>
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
                         <a href="{{ route('users.index') }}"
                             class="nav-link @isset($navlink) @if (in_array('pengguna', $navlink)) active @endif @endisset">
                             <i class="nav-icon fas fa-user"></i>
-                            <p>PENGGUNA </p>
+                            <p>USERS</p>
                         </a>
                     </li>
-                </ul>
-            </li>
-        @endcan
-
-        @can('role-list')
-            <li
-                class="nav-item @isset($navlink) @if (in_array('pengurusan_sistem', $navlink)) menu-open @endif @endisset">
-                <a href="#"
-                    class="nav-link @isset($navlink) @if (in_array('pengurusan_sistem', $navlink)) active @endif @endisset">
-                    <i class="nav-icon fas fa-tools"></i>
-                    <p>PENGURUSAN SISTEM<i class="fas fa-angle-left right"></i></p>
-                </a>
-                <ul class="nav nav-treeview">
                     <li class="nav-item">
                         <a href="{{ route('roles.index') }}"
                             class="nav-link @isset($navlink) @if (in_array('peranan', $navlink)) active @endif @endisset">
                             <i class="nav-icon fas fa-suitcase-rolling"></i>
-                            <p>PERANAN</p>
+                            <p>ROLES</p>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a href="{{ route('permissions.index') }}"
                             class="nav-link @isset($navlink) @if (in_array('kebenaran', $navlink)) active @endif @endisset">
                             <i class="nav-icon far fa-calendar-alt"></i>
-                            <p> KEBENARAN </p>
+                            <p>PERMISSIONS</p>
                         </a>
                     </li>
                 </ul>
             </li>
         @endcan
+
         {{-- Projects --}}
         <li class="nav-item">
             <a href="{{ route('projects.index') }}" class="nav-link">
