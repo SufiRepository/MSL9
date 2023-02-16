2ac8d3c5b3cb4c12186fa9d54e927ab9c95d0f7e
Ahmad Sufi Bin Abdul Hamid <sufi@mindwave.my>
2023-02-16 12:26:05 +0800

    added base blade

--- resources/views/auth/pwalogin.blade.php
+++ resources/views/auth/pwalogin.blade.php
@@ -5,19 +5,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }}</title>
-
+    @laravelPWA
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    {{-- <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ URL::asset('adminlte3/plugins/fontawesome-free/css/all.min.css') }}" />
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ URL::asset('adminlte3/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}" />
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ URL::asset('adminlte3/dist/css/adminlte.min.css') }}" />
+
+    <style>
+        .login-background {
+            background-image: url("{{ asset('images') }}/citybg.jpg");
+            background-size: cover;
+        }
+    </style>
+
 </head>
 
-<body class="hold-transition login-page">
+<body class="hold-transition login-page login-background">
     <div class="login-box">
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
---
+++ resources/views/layouts/headlogin.blade.php
@@ -0,0 +1,16 @@
+<!-- Google Font: Source Sans Pro -->
+{{-- <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> --}}
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ URL::asset('adminlte3/plugins/fontawesome-free/css/all.min.css') }}" />
+<!-- icheck bootstrap -->
+<link rel="stylesheet" href="{{ URL::asset('adminlte3/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}" />
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ URL::asset('adminlte3/dist/css/adminlte.min.css') }}" />
+
+<style>
+    .login-background {
+        background-image: url("{{ asset('images') }}/citybg.jpg");
+        background-size: cover;
+    }
+</style>
